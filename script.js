ae68999f09d2d2cdc3cb311fd0cb824fb5caafae
Katherine Jones <kejone1414@gmail.com>
2020-03-26 22:03:10 -0400

    commenting out randomizing the questions.  will come back to it if I have time

--- script.js
+++ script.js
@@ -23,104 +23,106 @@ var i = 0;
 //storing questions and answers
 var questionArr = [
     {
-      question: "Is JavaScript a case-sensitive language?",
-      answers: {
-        a: "Yes",
-        b: "No",
-      },
-      correctAnswer: "a"
+        question: "Is JavaScript a case-sensitive language?",
+        answers: {
+            a: "Yes",
+            b: "No",
+            c: "It depends",
+            d: "Maybe"
+        },
+        correctAnswer: "a"
     },
     {
-      question: "Which of the following is true about cookie handling in JavaScript?",
-      answers: {
-        a: "JavaScript can manipulate cookies using the cookie property of the Document object.",
-        b: "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.",
-        c: "Both of the above.",
-        d: "None of the above."
-      },
-      correctAnswer: "c"
+        question: "Which of the following is true about cookie handling in JavaScript?",
+        answers: {
+            a: "JavaScript can manipulate cookies using the cookie property of the Document object.",
+            b: "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.",
+            c: "Both of the above.",
+            d: "None of the above."
+        },
+        correctAnswer: "c"
     },
     {
-      question: "Which of the following is correct about callbacks?",
-      answers: {
-        a: "A callback is a plain JavaScript function passed to some method as an argument or option.",
-        b: "Some callbacks are just events, called to give the user a chance to react when a certain state is triggered.",
-        c: "Both of the above.",
-        d: "None of the above."
-      },
-      correctAnswer: "c"
+        question: "Which of the following is correct about callbacks?",
+        answers: {
+            a: "A callback is a plain JavaScript function passed to some method as an argument or option.",
+            b: "Some callbacks are just events, called to give the user a chance to react when a certain state is triggered.",
+            c: "Both of the above.",
+            d: "None of the above."
+        },
+        correctAnswer: "c"
     },
     {
         question: "Which built-in method returns the calling string value converted to upper case?",
         answers: {
-          a: "toUpperCase()",
-          b: "toUpper()",
-          c: "changeCase(case)",
-          d: "None of the above."
+            a: "toUpperCase()",
+            b: "toUpper()",
+            c: "changeCase(case)",
+            d: "None of the above."
         },
         correctAnswer: "a"
     },
     {
         question: "Which of the following function of Boolean object returns a string containing the source of the Boolean object?",
         answers: {
-          a: "toSource()",
-          b: "valueOf()",
-          c: "toString()",
-          d: "None of the above."
-    },
+            a: "toSource()",
+            b: "valueOf()",
+            c: "toString()",
+            d: "None of the above."
+        },
         correctAnswer: "a"
     },
     {
         question: "Which of the following function of String object extracts a section of a string and returns a new string?",
         answers: {
-          a: "slice()",
-          b: "split()",
-          c: "replace()",
-          d: "search()"
+            a: "slice()",
+            b: "split()",
+            c: "replace()",
+            d: "search()"
         },
         correctAnswer: "a"
     },
     {
         question: "Which of the following function of String object creates an HTML anchor that is used as a hypertext target?",
         answers: {
-          a: "anchor()",
-          b: "link()",
-          c: "blink()",
-          d: "big()"
-    },
+            a: "anchor()",
+            b: "link()",
+            c: "blink()",
+            d: "big()"
+        },
         correctAnswer: "a"
     },
     {
         question: "Which of the following function of String object causes a string to be displayed as struck-out text, as if it were in a <strike> tag?",
         answers: {
-          a: "sup()",
-          b: "small()",
-          c: "strike()",
-          d: "sub()"
-    },
+            a: "sup()",
+            b: "small()",
+            c: "strike()",
+            d: "sub()"
+        },
         correctAnswer: "c"
     },
     {
         question: "Which of the following function of Array object joins all elements of an array into a string?",
         answers: {
-          a: "concat()",
-          b: "join()",
-          c: "pop()",
-          d: "map()"
-    },
+            a: "concat()",
+            b: "join()",
+            c: "pop()",
+            d: "map()"
+        },
         correctAnswer: "b"
     },
     {
         question: "Which of the following function of Array object removes the first element from an array and returns that element?",
         answers: {
-          a: "reverse()",
-          b: "shift()",
-          c: "slice()",
-          d: "some()"
-    },
+            a: "reverse()",
+            b: "shift()",
+            c: "slice()",
+            d: "some()"
+        },
         correctAnswer: "b"
-    },          
-  ];
+    },
+];
 
 // start timer and quiz
 $(startBtn).click(function startFn() {
@@ -141,61 +143,60 @@ $(startBtn).click(function startFn() {
         };
     };
 
+    // shuffleArray(questionArr);
     quizFn();
 
 });
 
 //grab index of question randomly
-function questionRandom() {
-    i = (Math.floor(Math.random() * questionArr.length));
-    return i;
-};
+// function questionRandom() {
+//     i = (Math.floor(Math.random() * questionArr.length));
+//     return i;
+// };
+
+// Durstenfeld shuffle to shuffle array
+// function shuffleArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         var j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     return array;
+// }
 
 
 function quizFn() {
-    questionRandom();
 
-    //add text of question to div
-    $(header).text(questionArr[i].question);
-    $(header).css("font-size", "30px");
-
-    //add text to buttons if that question has that answer choice
-    if (questionArr[i].answers.a !== null) {
+    for (i = 0; i < questionArr.length; i++) {
+        //add text of question to div
+        $(header).text(questionArr[i].question);
+        $(header).css("font-size", "30px");
+    
+        //add text to buttons
         $(choicea).text("a. " + questionArr[i].answers.a);
         $("#btn0").attr("data-answer", "a");
         $("#btn0").css("display", "block");
-    };
-    if (questionArr[i].answers.b !== null) {
+    
         $(choiceb).text("b. " + questionArr[i].answers.b);
         $("#btn1").attr("data-answer", "b");
         $("#btn1").css("display", "block");
-    };
-    if (questionArr[i].answers.c !== null) {
+    
         $(choicec).text("c. " + questionArr[i].answers.c);
         $("#btn2").attr("data-answer", "c");
         $("#btn2").css("display", "block");
-    };
-    if (questionArr[i].answers.d !== null) {
+    
         $(choiced).text("d. " + questionArr[i].answers.d);
         $("#btn3").attr("data-answer", "d");
         $("#btn3").css("display", "block");
-    };
-
-    $(".btn").click(function userChoice() {
-        var userPick = $(this).attr("data-answer");
-
-        if (userPick === questionArr[i].correctAnswer) {
-            $(result).text("Correct!");
-        } else {
-            $(result).text("Wrong!");
-        };
-    })
-
-    questionArr[i].splice(i, 1,)
+    
+        $(".btn").click(function userChoice() {
+            var userPick = $(this).attr("data-answer");
+    
+            if (userPick === questionArr[i].correctAnswer) {
+                $(result).text("Correct!");
+            } else {
+                $(result).text("Wrong!");
+            };
+        })
+    }
 }
 
-// answers: {
-//     a: "Yes",
-//     b: "No",
-//   },
-//   correctAnswer: "a"
